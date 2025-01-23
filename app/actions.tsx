27e5dc827fa4b9dfc462212a97ace41c84bad685
Aden Fernandes <adenferns999@gmail.com>
2025-01-24 00:05:43 +0530

    feat: Added framer-motion
    - Added framer-motion animation to each card.

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,8 +1,11 @@
 'use server';
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 
 export const getAnimeDetails = async(page : number) => {
     const animeFetch = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`);
 
     const data = await animeFetch.json();
-    return data;
+    return data.map((item: AnimeProp, index:number) => (
+        <AnimeCard key={item.id} anime={item} index={index} />
+      ))
 }
